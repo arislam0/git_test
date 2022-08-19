6e054c902ae5be7df794578630aca9a289dc5311
arislam0 <mdarifislam945@gmail.com>
2022-08-20 01:50:49 +0600

    js-add

---
+++ script.js
@@ -0,0 +1,10 @@
+var numbers = [1, 2, 3, 4, 5, 6, 7];
+//routine to calculate the sum
+function sum(n)
+{
+	var sumation = 0;
+	// sumatiom
+	sumation = n.reduce( function (0,n) { return (0+n);});
+	return sumation;
+}
+console.log(sum(numbers));
